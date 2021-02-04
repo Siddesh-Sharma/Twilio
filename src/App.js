2792ddc9d49e94ef3159edcfb124a0b7afb82572
siddesh SHARMA <siddeshsharma1999@gmail.com>
2021-02-04 17:23:12 +0530

    inatalized immer integration to our project to mantain local state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Login from './components/Login';
+import {useImmer} from 'use-immer';
 
 function App() {
-  return <div><Login /></div>
-    
   
+  const [user, setUser] = useImmer({
+      username: '',
+      mobileNumber: '',
+  });
+  
+  return (<div>
+            <Login user={user} setUser={setUser}/>
+          </div>
+    
+  );
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import {Button, Form, Grid, Header, Segment} from 'semantic-ui-react'
 
-function Login() {
+function Login({user:{username, mobileNumber}, setUser}) {
+    function populateFields(event, data){
+        setUser(draft => {
+            draft[data.name] = data.value;
+        });
+    }
+    
+    
     return <Grid textAlign="center" verticalAlign="middle" style={{height: '100vh'}}>
                 <Grid.Column style={{maxWidth: 450}}>
-                    <Header as='h2' color='teal' textAlign='centre'>Log into your account</Header>
+                    <Header 
+                        as='h2' 
+                        color='teal' 
+                        textAlign='centre'
+                    >
+                        Log into your account
+                    </Header>
                     <Form>
                         <Segment stacked>
-                            <Form.Input fluid icon="user" iconPosition='left' placeholder="Username">
+                            <Form.Input 
+                                fluid icon="user" 
+                                iconPosition='left' 
+                                placeholder="Username" 
+                                value={username}
+                                onChange={(event, data) => populateFields(event, data)}
+                                name="username"
+                            >
                             </Form.Input>
-                            <Form.Input fluid icon="mobile alternate" iconPosition='left' placeholder="Mobile Number" />
-                            <Button color='teal' fluid size='large'>
+                            <Form.Input 
+                                fluid icon="mobile alternate" 
+                                iconPosition='left' 
+                                placeholder="Mobile Number"
+                                value={mobileNumber}
+                                onChange={(event, data) => populateFields(event, data)} 
+                                name="mobileNumber"
+                            />
+                            <Button 
+                                color='teal' 
+                                fluid size='large'
+                            >
                                 Login/Signup
                             </Button>
                         </Segment>
